0d5464c282c8e062e15325d2ea08040db44228ad
Dread <dread@start9labs.com>
2022-05-25 17:45:05 -0400

    added NODE_OPTIONS var

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -11,12 +11,14 @@ chmod -R a+x /root/.bos
 mv ../credentials.json /root/.bos/embassy/credentials.json
 chmod a+x /root/.bos/embassy/credentials.json
 echo "export BOS_DEFAULT_SAVED_NODE=embassy" >> /etc/bash.bashrc
+echo "export NODE_OPTIONS=--openssl-legacy-provider" >> /etc/bash.bashrc
 source /etc/bash.bashrc
 echo "Checking for peers..."
 
 # Starting command line
 while true;
 do export BOS_DEFAULT_SAVED_NODE=embassy;
+export NODE_OPTIONS=--openssl-legacy-provider;
 bos peers;
 sleep 20000;
 done
